fcd5e98c68d2f44d7602393b9effe7bd1e03cecb
Kathiani <kathianisouza@gmail.com>
2024-03-30 22:30:05 -0300

    sending data from InterSCity to MidVAL

--- src/main/java/com/example/controller/controlrequests/RequestsController.java
+++ src/main/java/com/example/controller/controlrequests/RequestsController.java
@@ -1,26 +1,42 @@
 package com.example.controller.controlrequests;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
-public class RequestsController{   //*simulando método chamado pela plataforma para acesso a dados dos recursos
+public class RequestsController{   
     
   
     @GetMapping("/resources_data")   
-    public static void getResourceData(){
+    public static void getResourceData(){  //*simulando função da InterScity: http://10.10.10.104:8000/collector/resources/
     //info = resource location or resource description valida = if data from resource needs to be validated; 
-    String uuid = "9cf609af-3e7d-4bde-adad-f8b6f2dbe297"; //fixando uuid por enquanto
-    String endpointInterSCity = "http://10.10.10.104:8000/catalog/resources/";  // Acessando dados da plataforma via http ao invés de instrumentar validador no DataCollector (resources_data)
-    RestTemplate restTemplate = new RestTemplate();
-    String responseDataResource = restTemplate.getForObject(endpointInterSCity, String.class); 
 
-    String endpointValidation = "http://localhost:8080/midval/validate" + responseDataResource ; 
-    String responseValidation = restTemplate.getForObject(endpointValidation, String.class); 
+    RestTemplate restTemplate = new RestTemplate();
+    String uuid = "9cf609af-3e7d-4bde-adad-f8b6f2dbe297"; // *fixando uuid por enquanto
+    String endpointInterSCity = "http://10.10.10.104:8000/collector/resources/" + uuid + "/data";  // Acessando dados na plataforma via http ao invés de instrumentar validador no DataCollector (resources_data)
+    String responseDataResource = restTemplate.getForObject(endpointInterSCity, String.class); 
+    String endpointMidval = "http://localhost:8080/midval/validate";   // *codificar url devido a caracteres especiais;
+  
+    HttpHeaders headers = new HttpHeaders();
+	headers.setContentType(MediaType.APPLICATION_JSON);
+	HttpEntity<String> requestEntity = new HttpEntity<>(responseDataResource, headers);
+	ResponseEntity<String> responseValidation = restTemplate.postForEntity(endpointMidval, requestEntity, String.class); 
+    //String responseValidation = restTemplate.getForObject(endpointMidval, String.class); 
+
+
+
+
+
+    
     //abrir conexão com midVal e passar o dado;
     //repassar dado pra app - essa informação você apresenta na tela;
-    System.out.println(responseValidation);
-    //System.out.println(endpointValidation);
+    //System.out.println(responseValidation);
+    //System.out.println(responseValidation);
 
     }
 
--- src/main/java/com/example/controller/controlrequests/StartupDataRequests.java
+++ src/main/java/com/example/controller/controlrequests/StartupDataRequests.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SimulateRequestsApplication {
+public class StartupDataRequests {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SimulateRequestsApplication.class, args);
+		SpringApplication.run(StartupDataRequests.class, args);
 	}
 
 }
