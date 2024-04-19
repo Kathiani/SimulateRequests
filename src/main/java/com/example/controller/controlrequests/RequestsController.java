e8b03bbed20320ff4b03896ae9bd9236a5544957
Kathiani <kathianisouza@gmail.com>
2024-04-18 23:42:57 -0300

    adding schedule for request sequence

--- src/main/java/com/example/controller/controlrequests/RequestsController.java
+++ src/main/java/com/example/controller/controlrequests/RequestsController.java
@@ -3,28 +3,30 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 
-
 @RestController
 public class RequestsController{   
-    
+  
+    @Scheduled(fixedRate = 5000) 
     @GetMapping("/sensor-discoverer")  // #simulando consulta na plataforma via midval
-    public static String SensorValueDiscoverer(){
+    public static void SensorValueDiscoverer(){
         RestTemplate restTemplate = new RestTemplate();
-        String endpointValida = "http://localhost:8080/midval/search-validate";  // para uso: plataforma define um endpoint de validação com acesso ao midval ex: http://10.10.10.104:8000/collector/valid_resources_data
-        String responseValidData = restTemplate.getForObject(endpointValida, String.class);
-        return responseValidData; 
+       // String endpointValida = "http://localhost:8080/midval/search-validate";  // para uso: plataforma define um endpoint de validação com acesso ao midval ex: http://10.10.10.104:8000/collector/valid_resources_data
+       // String responseValidData = restTemplate.getForObject(endpointValida, String.class);
+       // return responseValidData; 
+       System.err.println("Teste");
     
     }
 
     @GetMapping("/resources-data")   
     public static String getResourceData(){  // #simulando consulta no banco da plataforma e repassando dado para midlval #roundtrip
         RestTemplate restTemplate = new RestTemplate();
-        String uuid = "9cf609af-3e7d-4bde-adad-f8b6f2dbe297"; 
+        String uuid = "9cf609af-3e7d-4bde-adad-f8b6f2dbe297";   //resources-data
         String endpointInterSCity = "http://10.10.10.104:8000/collector/resources/" + uuid + "/data";  
         String responseDataResource = restTemplate.getForObject(endpointInterSCity, String.class); 
         String endpointMidval = "http://localhost:8080/midval/validate";  
@@ -32,7 +34,7 @@ public class RequestsController{
         headers.setContentType(MediaType.APPLICATION_JSON);
         HttpEntity<String> requestEntity = new HttpEntity<>(responseDataResource, headers);  // enviando dado recuperado para validação
         ResponseEntity<String> responseValidation = restTemplate.postForEntity(endpointMidval, requestEntity, String.class); 
-       return "O dado validado pronto para app a é igual a:" + responseValidation; 
+       return "Resultado da validação é:" + responseValidation; 
 
 
     }
